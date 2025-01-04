3bc6e77822a9c78d840ffc257ba4d8c0150d7d72
unknown <dineshnadipalli411@gmail.com>
2025-01-04 12:39:06 +0530

    file 1

---
+++ src/session_3.java
@@ -0,0 +1,13 @@
+
+public class session_3{
+    public static void main(String args[]){
+        String str="hello";
+        int len =str.length();
+        System.out.println(str.length());
+        System.out.println(str.charAt(len - 1));
+        System.out.println(str.substring(len - 1));
+        System.out.println(str.substring(beginIndex:1,endIndex:4));
+
+
+    }
+}
