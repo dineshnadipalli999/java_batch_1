fe29d9b372918e28a9727453dde72f20efe3ee57
dineshnadipalli999 <dineshnadipalli411@gmail.com>
2025-01-09 18:58:51 +0530

    Add files via upload

---
+++ src/DAY_7.java
@@ -0,0 +1,24 @@
+public class DAY_7 {
+    public static void main(String args[]){
+        class Solution {
+            public boolean containsDuplicate(int[] nums) {
+                for(int i=0;i<nums.length;i++){
+                    for(int j=i+1;j<nums.length;j++){
+                        if(nums[i] == nums[j]){
+                            return true;
+                        }
+                       
+                         
+                    }
+                    
+                    
+                }
+        
+                return false;
+               }
+        }
+        
+        
+
+    }
+}
